9438ab3c8566caf130e46f4ed9da490e5670c458
Sung Jun Cheon <ultrastrange@gmail.com>
2025-04-11 14:49:32 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="tokenizers",
     version="0.0.0",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[],
 )
